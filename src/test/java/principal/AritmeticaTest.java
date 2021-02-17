8b14c2bbdaa50ceb0e6c08af7a45ded4a103e455
sose1102000 <sose1102@gmail.com>
2021-02-17 13:57:51 +0100

    Creada estructura del proyecto

---
+++ src/test/java/principal/AritmeticaTest.java
@@ -0,0 +1,23 @@
+package principal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AritmeticaTest {
+
+    @org.junit.jupiter.api.Test
+    void suma() {
+        fail("Test no superado");
+    }
+
+    @org.junit.jupiter.api.Test
+    void resta() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiplicacion() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void division() {
+    }
+}
